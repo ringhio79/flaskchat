5b20b9b4fe47aa25e47fc8c0c9b7678b90daf3a4
Giselle <gigi108@gmail.com>
2018-07-24 20:43:59 +0000

    phase 1 of tag msgs

--- flaskchat.py
+++ flaskchat.py
@@ -4,9 +4,7 @@ import os
 app = Flask(__name__)
 
 chat_history=[
-    "Bruce: hello how are you?",
-    "Tori: good thank you and you?",
-    "Bruce: let's go have lunch"
+    
     ]
 
 @app.route("/")
@@ -18,16 +16,38 @@ def do_join():
     username = request.args['username']
     return redirect("/chat/" + username)
 
+def can_view(message, username):
+        addressed_to_me = "@" + username in message
+        addressed_to_nobody = "@" not in message
+        i_wrote_it = message.startswith(username + ":")
+        return addressed_to_me or addressed_to_nobody or i_wrote_it
+
+
 @app.route("/chat/<username>")
 def show_chat(username):
-    return render_template("chat.html", chat_history=chat_history, username=username)
+    filter_chat_history = []
+    for message in chat_history:
+        if can_view(message, username):
+            filter_chat_history.append(message)
+
+    return render_template("chat.html", chat_history=filter_chat_history, username=username)
+
+@app.route("/tags/brexit")
+def show_tags():
+    tag_messages = []
+    for message in chat_history:
+        if "#brexit" in message:
+            tag_messages.append(message)
+            
+    return render_template("brexit.html", tag_messages=tag_messages)
+
 
 @app.route("/new", methods=['POST'])
-def add_item():
+def add_new_message():
     message = request.form['message']
     username= request.form['username']
     chat_history.append(username + ": " + message)
-    return redirect("/chat/username")
+    return redirect("/chat/" + username)
 
 
 if __name__ == "__main__":
